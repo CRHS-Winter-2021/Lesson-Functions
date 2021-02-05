acabaaedf821e9f5e1550faeb2e2d9413284e8a7
MrAPauls <55466660+MrAPauls@users.noreply.github.com>
2021-02-05 15:11:18 -0400

    Update Functions_Lesson.py

--- Functions_Lesson.py
+++ Functions_Lesson.py
@@ -2,15 +2,15 @@
 ##Functions
 
 #Four reasons to use functions
-#1.
-#2.
-#3.
-#4.
+#1. Organize programs by grouping chucks of code
+#2. reUSE code in multiple places or SHARE with friends
+#3. Improves READIBILITY and TESTING
+#4. 
 
 #For the following function..
-#argument:
-#parameter:
-#results:
+#arguments: 3 and 4
+#parameters:  a and b 
+#results: x will be 7
 def add_two(a, b):
     x = a + b
     return x
@@ -19,3 +19,12 @@ add_two(3, 4)
 
 
 ##
+def my_ave(*args):
+    #print(type(args))
+    count = 0
+    sum = 0
+    for n in args:
+        count += 1
+        sum += n
+    return(sum / count)
+
